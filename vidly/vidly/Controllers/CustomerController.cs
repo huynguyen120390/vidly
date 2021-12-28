117fee3fce78496a68de711ad5d71bd93543abd6
Huy Nguyen <hnguyen@kymetacorp.com>
2021-12-28 15:02:29 -0800

    First Migration

---
+++ vidly/vidly/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customerList = new List<Customer>()
+            {
+                new Customer() {Id = 0, Name = "Huy"},
+                new Customer() {Id = 1, Name = "Ai"}
+            };
+            return View(customerList);
+        }
+
+        //GET: Customer/Details/1
+        public ActionResult Details(int id)
+        {
+            var customerList = new List<Customer>()
+            {
+                new Customer() {Id = 0, Name = "Huy"},
+                new Customer() {Id = 1, Name = "Ai"}
+            };
+            if (id >= customerList.Count)
+            {
+                return HttpNotFound();
+            }
+            return View(customerList[id]);
+        }
+    }
+}
---
+++ vidly/vidly/Controllers/MovieController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customerList = new List<Movie>()
+            {
+                new Movie() {Id = 0, Name = "Con co be be"},
+                new Movie() {Id = 1, Name = "Con chim non"}
+            };
+            return View(customerList);
+        }
+
+        //GET: Customer/Details/1
+        public ActionResult Details(int id)
+        {
+            var customerList = new List<Movie>()
+            {
+                new Movie() {Id = 0, Name = "Con co be be"},
+                new Movie() {Id = 1, Name = "Con chim non"}
+            };
+            if (id >= customerList.Count)
+            {
+                return HttpNotFound();
+            }
+            return View(customerList[id]);
+        }
+    }
+}
