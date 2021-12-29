baeae4ab0f81918476d84faedb4d614575d0a03b
Huy Nguyen <hnguyen@kymetacorp.com>
2021-12-28 18:36:18 -0800

    Add Discount Rate

--- vidly/vidly/Controllers/CustomerController.cs
+++ vidly/vidly/Controllers/CustomerController.cs
@@ -3,36 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using vidly.Models;
 
 namespace vidly.Controllers
 {
     public class CustomerController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customer
         public ActionResult Index()
         {
-            var customerList = new List<Customer>()
-            {
-                new Customer() {Id = 0, Name = "Huy"},
-                new Customer() {Id = 1, Name = "Ai"}
-            };
-            return View(customerList);
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
         }
 
         //GET: Customer/Details/1
         public ActionResult Details(int id)
         {
-            var customerList = new List<Customer>()
-            {
-                new Customer() {Id = 0, Name = "Huy"},
-                new Customer() {Id = 1, Name = "Ai"}
-            };
-            if (id >= customerList.Count)
-            {
-                return HttpNotFound();
-            }
-            return View(customerList[id]);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null) return HttpNotFound();
+            return View(customer);
         }
     }
 }
--- vidly/vidly/Models/Customer.cs
+++ vidly/vidly/Models/Customer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Web;
 
 namespace vidly.Models
@@ -8,7 +10,13 @@ namespace vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        
+        public  bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
 
     }
 }
