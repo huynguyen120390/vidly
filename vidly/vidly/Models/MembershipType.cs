cf4815d5a075327ad716599d3711ddc28be4852a
Huy Nguyen <hnguyen@kymetacorp.com>
2021-12-28 20:12:37 -0800

    Add Name To MembershipType

--- vidly/vidly/Models/MembershipType.cs
+++ vidly/vidly/Models/MembershipType.cs
@@ -11,5 +11,6 @@ namespace vidly.Models
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
         public byte Id { get; set; }
+        public string Name { get; set; }
     }
 }
