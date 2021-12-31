78180c3112c687b9bbd4094179a0a17f52f9107e
Huy Nguyen <hnguyen@kymetacorp.com>
2021-12-30 21:39:13 -0800

    Add Movie Web Api

--- vidly/vidly/Controllers/Api/CustomerController.cs
+++ vidly/vidly/Controllers/Api/CustomerController.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Web.Http;
+using AutoMapper;
+using vidly.Dtos;
 using vidly.Models;
 
 namespace vidly.Controllers.Api
@@ -19,13 +21,13 @@ namespace vidly.Controllers.Api
         }
 
         //GET /api/customers
-        public IEnumerable<Customer> GetCustomers()
+        public IEnumerable<CustomerDto> GetCustomers()
         {
-            return _context.Customers.ToList();
+            return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
         }
 
         //GET /api/customers/1
-        public Customer GetCustomer(int id)
+        /*public CustomerDto GetCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
@@ -33,26 +35,55 @@ namespace vidly.Controllers.Api
                 throw new HttpResponseException(HttpStatusCode.NotFound);
             }
 
-            return customer;
+            return Mapper.Map<Customer, CustomerDto>(customer);
+        }*/
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
         }
 
-        //POST /api/customers
+        /*//POST /api/customers
         [HttpPost]
-        public Customer PostCustomer(Customer customer)
+        public CustomerDto PostCustomer(CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
             {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
 
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
             _context.Customers.Add(customer);
             _context.SaveChanges();
-            return customer;
+            customerDto.Id = customer.Id;
+            return customerDto;
+        }*/
+
+        [HttpPost]
+        public IHttpActionResult PostCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            customerDto.Id = customer.Id;
+            return Created(new Uri(Request.RequestUri  + "/" + customer.Id), customerDto);
         }
-        
+
+
+
         //PUT /api/customer/1
         [HttpPut]
-        public void UpdateCustomer(int id, Customer customer)
+        public void UpdateCustomer(int id, CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
             {
@@ -65,10 +96,13 @@ namespace vidly.Controllers.Api
                 throw new HttpResponseException(HttpStatusCode.NotFound);
             }
 
-            customerInDb.Name = customerInDb.Name;
-            customerInDb.BirthDate = customerInDb.BirthDate;
-            customerInDb.IsSubscribedToNewsLetter = customerInDb.IsSubscribedToNewsLetter;
-            customerInDb.MemberShipTypeId = customerInDb.MemberShipTypeId;
+            Mapper.Map(customerDto, customerInDb);
+    /*
+            With Mapper we dont need the following assignments
+            customerInDb.Name = customerDto.Name;
+            customerInDb.BirthDate = customerDto.BirthDate;
+            customerInDb.IsSubscribedToNewsLetter = customerDto.IsSubscribedToNewsLetter;
+            customerInDb.MemberShipTypeId = customerDto.MemberShipTypeId;*/
             _context.SaveChanges();
         }
 
---
+++ vidly/vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using vidly.Models;
+
+namespace vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public byte MemberShipTypeId { get; set; }
+        //[Min18YearsIfMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
